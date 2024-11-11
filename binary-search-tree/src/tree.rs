f1d031379d07bdd0b83021b83e7dd8c1e863b975
Kevin Day <kevinday.home@gmail.com>
2024-11-11 16:07:19 -0600

    add drop impl, fix warning, fix unit test flub

--- binary-search-tree/src/tree.rs
+++ binary-search-tree/src/tree.rs
@@ -3,6 +3,7 @@ use std::ptr::NonNull;
 
 type UsizeLink = Option<NonNull<UsizeNode>>;
 
+#[derive(Debug)]
 struct UsizeNode
 {
     elem: usize,
@@ -19,21 +20,48 @@ pub struct UsizeTree
 impl UsizeTree
 {
     // create a new empty tree
-    pub fn new() -> Self
-    {
-        todo!();
-    }
+    pub fn new() -> Self { Self { root: None, len: 0 } }
 
     // returns the number of elements in the tree
-    pub fn len(&self) -> usize
-    {
-        todo!();
-    }
+    pub fn len(&self) -> usize { self.len }
 
     // returns whether tree is empty or not
-    pub fn is_empty(&self) -> bool
+    pub fn is_empty(&self) -> bool { self.len == 0 }
+
+    fn find_link_mut(&mut self, elem: usize) -> &mut UsizeLink
     {
-        todo!();
+        unsafe {
+            let mut cur_node = &mut self.root;
+            while let Some(cur) = cur_node
+            {
+                let parent = &mut (*cur.as_ptr());
+                if elem == parent.elem
+                {
+                    return cur_node;
+                }
+
+                cur_node = if elem < parent.elem { &mut parent.left } else { &mut parent.right };
+            }
+            cur_node
+        }
+    }
+
+    fn find_link(&self, elem: usize) -> &UsizeLink
+    {
+        unsafe {
+            let mut cur_node = &self.root;
+            while let Some(cur) = cur_node
+            {
+                let parent = &mut (*cur.as_ptr());
+                if elem == parent.elem
+                {
+                    return cur_node;
+                }
+
+                cur_node = if elem < parent.elem { &mut parent.left } else { &mut parent.right };
+            }
+            cur_node
+        }
     }
 
     // insert an element into the tree
@@ -41,32 +69,98 @@ impl UsizeTree
     // insertion fails if the value is already in the tree
     pub fn insert(&mut self, elem: usize) -> bool
     {
-        todo!();
+        unsafe {
+            let insert_link = self.find_link_mut(elem);
+            if insert_link.is_some()
+            {
+                // don't insert duplicates
+                return false;
+            }
+
+            let new_node = UsizeNode { elem, left: None, right: None };
+            let new = Some(NonNull::new_unchecked(Box::into_raw(Box::new(new_node))));
+            *insert_link = new;
+            self.len += 1;
+            return true;
+        }
     }
 
     // search the tree for a value equal to passed elem
     // returns whether equal value was found or not
-    pub fn contains(&self, elem: &usize) -> bool
-    {
-        todo!();
-    }
+    pub fn contains(&self, elem: &usize) -> bool { self.find_link(*elem).is_some() }
 
     // remove an element from the tree
     // return if removal was successful
     // removal fails if the value isn't in the tree
     pub fn remove(&mut self, elem: usize) -> bool
     {
-        todo!();
+        unsafe {
+            let remove_link = self.find_link_mut(elem);
+
+            if remove_link.is_some()
+            {
+                let remove_node = &mut (*remove_link.unwrap().as_ptr());
+
+                if remove_node.left.is_none()
+                {
+                    let old = Box::from_raw(remove_link.unwrap().as_ptr());
+                    *remove_link = remove_node.right.take();
+                    drop(old);
+                }
+                else if remove_node.right.is_none()
+                {
+                    let old = Box::from_raw(remove_link.unwrap().as_ptr());
+                    *remove_link = remove_node.left.take();
+                    drop(old);
+                }
+                else
+                {
+                    let mut successor_link = &mut remove_node.right;
+                    while let Some(_) = (*successor_link.unwrap().as_ptr()).left
+                    {
+                        successor_link = &mut (*successor_link.unwrap().as_ptr()).left;
+                    }
+
+                    remove_node.elem = (*successor_link.unwrap().as_ptr()).elem;
+
+                    let successor_node = &mut (*successor_link.unwrap().as_ptr());
+                    let old = Box::from_raw(successor_link.unwrap().as_ptr());
+                    *successor_link = successor_node.right.take();
+                    drop(old);
+                }
+                self.len -= 1;
+                return true;
+            }
+
+            return false;
+        }
     }
 
     // clear the tree
     // postcondition: tree is empty
-    pub fn clear(&mut self) { todo!() }
+    pub fn clear(&mut self)
+    {
+        unsafe {
+            while let Some(root_node) = self.root
+            {
+                let root_elem = (*root_node.as_ptr()).elem;
+                self.remove(root_elem);
+            }
+        }
+    }
+}
+
+impl Drop for UsizeNode
+{
+    fn drop(&mut self)
+    {
+        println!("dropping: {:?}", self);
+    }
 }
 
 impl Drop for UsizeTree
 {
-    fn drop(&mut self) { todo!() }
+    fn drop(&mut self) { self.clear(); }
 }
 
 impl Clone for UsizeTree
@@ -79,7 +173,7 @@ impl Clone for UsizeTree
 
 impl Debug for UsizeTree
 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result
     {
         todo!();
     }
@@ -301,7 +395,7 @@ mod tests
         assert_eq!(tree.contains(&5), true);
         assert_eq!(tree.contains(&6), true);
         assert_eq!(tree.contains(&7), true);
-        assert_eq!(tree.len(), 6);
+        assert_eq!(tree.len(), 7);
 
         assert_eq!(tree.remove(5), true);
         assert_eq!(tree.contains(&5), false);
